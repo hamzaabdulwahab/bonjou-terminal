aeaf242dbd655920f355a54d07ac57a8b79fb91e
Hamza Wahab <hamzaabdulwahab19@gmail.com>
2025-11-05 16:11:18 +0500

    release: prepare v1.0.5

--- packaging/homebrew/bonjou.rb
+++ packaging/homebrew/bonjou.rb
@@ -1,11 +1,11 @@
 class Bonjou < Formula
   desc "Terminal-based LAN chat and transfer application"
   homepage "https://github.com/hamzawahab/bonjou-terminal"
-  version "1.0.4"
+  version "1.0.5"
 
   on_macos do
-    url "https://github.com/hamzaabdulwahab/bonjou-terminal/releases/download/v1.0.4/bonjou-macos.tar.gz"
-    sha256 "3c4d2e9a63bda8fc869372c29b291451e80764839d63953f13902b47256e369c"
+    url "https://github.com/hamzaabdulwahab/bonjou-terminal/releases/download/v1.0.5/bonjou-macos.tar.gz"
+    sha256 "d8d53297c6f186c2346891de58cfc898d3858d70b815ffe3c76a4a24c6f20496"
   end
 
   def install
